f8cdcb3381486ccc01929d9763e33c636164754b
progress0407 <progress0407@gmail.com>
2022-07-14 20:36:34 +0900

    to boot

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,9 +1,11 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        SpringApplication.run(Hello.class, args);
     }
 }
