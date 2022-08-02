5a175662b43fbe3cd84249c650af936c4ac8adeb
progress0407 <progress0407@gmail.com>
2022-08-02 21:00:13 +0900

    feat: yml 설정 추가 !

--- src/main/java/hello/controller/HelloController.java
+++ src/main/java/hello/controller/HelloController.java
@@ -6,12 +6,14 @@ import hello.repository.TeamRepository;
 import hello.repository.UserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class HelloController {
 
     private final UserRepository userRepository;
