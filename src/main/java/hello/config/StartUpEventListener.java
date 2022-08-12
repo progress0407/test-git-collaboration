ab6e44b4325899ca0a8ea6fca5979c38fde00ceb
progress0407 <progress0407@gmail.com>
2022-08-12 15:36:21 +0900

    feat 1

--- src/main/java/hello/config/StartUpEventListener.java
+++ src/main/java/hello/config/StartUpEventListener.java
@@ -31,6 +31,8 @@ public class StartUpEventListener implements ApplicationListener<ContextRefreshe
         log.info("---------------------------------");
         log.info("---------------------------------");
 
+        log.info("------------ feat 1-------------");
+
         initData();
     }
 
