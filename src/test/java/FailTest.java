f3f85e0cfbdbc1a7117ccfc85fe07ab840d39f13
progress0407 <progress0407@gmail.com>
2022-09-16 22:03:18 +0900

    udpate lines

---
+++ src/test/java/FailTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FailTest {
+
+    @Test
+    void test_fail() {
+        Assertions.assertTrue(false);
+    }
+}
