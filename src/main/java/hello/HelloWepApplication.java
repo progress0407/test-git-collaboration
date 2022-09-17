6db73529cbdbf5adf0a3fa5a3408b6231b3fe1dc
progress0407 <progress0407@gmail.com>
2022-09-17 10:37:18 +0900

    update workflows

--- src/main/java/hello/HelloWepApplication.java
+++ src/main/java/hello/HelloWepApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWepApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(HelloWepApplication.class, args);
     }
 }
